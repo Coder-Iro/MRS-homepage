fb015eb0a775bb7e3a7313c36b1162c5e6131bda
Coder-Iro <Coder-Iro@teamif.io>
2021-05-15 19:06:55 +0900

    Add SNS metatags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,51 @@
+import meta from './assets/meta.json'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mrs-homepage',
+    title: meta.title,
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ko',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { property: 'og:type', content: 'website' },
+      { name: 'theme-color', content: meta.color },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:title', name: 'twitter:title', content: meta.title },
+      { hid: 'og:title', property: 'og:title', content: meta.title },
+      { property: 'twitter:domain', content: meta.domain },
+      { hid: 'og:url', property: 'og:url', content: 'https://' + meta.domain },
+      {
+        hid: 'twitter:url',
+        property: 'twitter:url',
+        content: 'https://' + meta.domain,
+      },
+      { hid: 'description', name: 'description', content: meta.description },
+      {
+        hid: 'og:desciption',
+        property: 'og:description',
+        content: meta.description,
+      },
+      {
+        hid: 'twitter:desciption',
+        name: 'twitter:description',
+        content: meta.description,
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://' + meta.domain + meta.img,
+      },
+      {
+        hid: 'twitter:image',
+        property: 'twitter:image',
+        content: 'https://' + meta.domain + meta.img,
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
--- pages/index.vue
+++ pages/index.vue
@@ -31,7 +31,7 @@ import Vue from 'vue'
 export default Vue.extend({})
 </script>
 
-<style>
+<style lang="scss">
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
